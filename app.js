f7339bf4220082bc05bc42baefea87ebbf45c8d4
Jason Lau <jasonlau@Jasons-MacBook-Pro.local>
2023-10-02 20:06:53 -0400

    Fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
